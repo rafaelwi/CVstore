c61da0807af596c2c90b0210b3d8f5aa3b365f57
Scott <sweselan@uoguelph.com>
2022-01-08 12:39:07 -0500

    db table setup

--- db.py
+++ db.py
@@ -12,4 +12,36 @@ cur.execute('SELECT version()')
 db_version = cur.fetchone()
 print(db_version)
 
+def setup():
+    commands = (
+        """ CREATE TABLE IF NOT EXISTS company (
+                company_id integer PRIMARY KEY,
+                company_name VARCHAR(255) NOT NULL
+                )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS jobs (
+            job_id integer PRIMARY KEY,
+            company_id integer,
+            job_title VARCHAR(255),
+            application_url VARCHAR(1024),
+            salary integer,
+            CONSTRAINT fk_company
+                FOREIGN KEY(company_id) 
+	            REFERENCES company(customer_id)
+        )
+        """,
+        """ CREATE TABLE IF NOT EXISTS job_application (
+                app_id integer PRIMARY KEY,
+                company_id VARCHAR(255) NOT NULL,
+                job_id integer,
+                    CONSTRAINT fk_company
+                    FOREIGN KEY(company_id) 
+	                REFERENCES company(customer_id),
+                    CONSTRAINT fk_job
+                    FOREIGN KEY(job_id) 
+	                REFERENCES jobs(job_id)
+                )
+        """)
+
 def insert_job_data(id,author_id)
