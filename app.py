1ef6084a6df6b5cd6fe4c374e5954e76fbc77d5c
Scott <sweselan@uoguelph.com>
2022-01-08 16:43:35 -0500

    insert_job, edit_job, and remove_job

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-from db import setup
+from db import setup_db , insert_company, insert_job,edit_job,remove_job
 app = Flask(__name__)
 
-setup()
+setup_db()
+remove_job(1)
 @app.route('/')
 def hello():
     return 'Hello, World!'
--- db.py
+++ db.py
@@ -1,17 +1,20 @@
 import psycopg2
 from config import config
 
-def setup():
+def setup_db():
     print ("Setting up the database...")
     commands = (
+        """CREATE SEQUENCE IF NOT EXISTS company_id_seq;""",
+        """CREATE SEQUENCE IF NOT EXISTS job_id_seq;""",
+        """CREATE SEQUENCE IF NOT EXISTS job_application_id_seq;""",
         """ CREATE TABLE IF NOT EXISTS company (
-                company_id integer PRIMARY KEY,
+                company_id integer PRIMARY KEY DEFAULT nextval('company_id_seq'),
                 company_name VARCHAR(255) NOT NULL
                 )
         """,
         """
-        CREATE TABLE IF NOT EXISTS jobs (
-            job_id integer PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS job (
+            job_id integer PRIMARY KEY DEFAULT nextval('job_id_seq'),
             company_id integer,
             job_title VARCHAR(255),
             application_url VARCHAR(1024),
@@ -22,7 +25,7 @@ def setup():
         )
         """,
         """ CREATE TABLE IF NOT EXISTS job_application (
-                app_id integer PRIMARY KEY,
+                app_id integer PRIMARY KEY DEFAULT nextval('job_application_id_seq'),
                 company_id integer,
                 job_id integer,
                     CONSTRAINT fk_company
@@ -30,7 +33,7 @@ def setup():
 	                REFERENCES company(company_id),
                     CONSTRAINT fk_job
                     FOREIGN KEY(job_id) 
-	                REFERENCES jobs(job_id)
+	                REFERENCES job(job_id)
                 )
         """)
     conn = None
@@ -56,5 +59,91 @@ def setup():
         if conn is not None:
             conn.close()
 
+def insert_job(job_title,application_url,salary,company_id):
+    conn = None
+    sql = """INSERT INTO job (job_title,application_url,salary,company_id) VALUES (%s,%s,%s,%s);"""
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(sql, (job_title,application_url,salary,company_id))
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+        print ("Done!")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+       
+    finally:
+        if conn is not None:
+            conn.close()
+def edit_job( job_title,application_url,salary,company_id,job_id):
+    conn = None
+    sql = """UPDATE job SET job_title = %s, application_url = %s, salary = %s, company_id = %s where job_id = %s;"""
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(sql, (job_title,application_url,salary,company_id,job_id))
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+        print ("Done!")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+       
+    finally:
+        if conn is not None:
+            conn.close()
+def remove_job (job_id):
+    conn = None
+    sql = """DELETE FROM job WHERE job_id = %s;"""
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(sql, (job_id,))
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+        print ("Done!")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
+
+
+def insert_company(company_name):
+    conn = None
+    sql = """INSERT INTO company (company_name) SELECT %s WHERE NOT EXISTS ( SELECT company_name FROM company WHERE company_name = %s);"""
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(sql, (company_name,company_name))
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+        print ("Done!")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+       
+    finally:
+        if conn is not None:
+            conn.close()
 
