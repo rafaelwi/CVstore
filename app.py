0443de2c5aebb4873b9e7be8e31b8117aeff6bec
Scott <sweselan@uoguelph.com>
2022-01-08 19:42:19 -0500

    done

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
-from db import setup_db , insert_company, insert_job,edit_job,remove_job,remove_company
+from db import setup_db , insert_company, insert_job,edit_job,remove_job,remove_company,insert_job_app,remove_job_app
 app = Flask(__name__)
 
 setup_db()
-insert_company("Wigger")
+#insert_company("Facebook")
+#insert_job("Pussy magnet","http://facebook.com",6969,4)
+#insert_job_app(3,1)
+remove_job_app(5)
 @app.route('/')
 def hello():
     return 'Hello, World!'
